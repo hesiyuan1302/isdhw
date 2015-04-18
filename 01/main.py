06e9e64592c330c733e31e81efc8f4d0f73a84a3
hesiyuan1302 <1032621474@qq.com>
2015-04-18 15:30:06 +0800

    01

--- 01/main.py
+++ 01/main.py
@@ -3,4 +3,12 @@ for i in range(1, 10) :
         print ('%d x %d = %d\t' %(i, j, j*i), end ='')
         if (j==9): 
               print('\n')
-        
+      k=1
+      while(k<i+1):
+            print(end='                ')
+            k=k+1
+                    
+                    
+              
+              
+      
