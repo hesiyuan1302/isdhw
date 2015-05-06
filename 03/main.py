b2b6593fa291d9c114160416173b5a96d77acc56
hesiyuan1302 <1032621474@qq.com>
2015-05-06 20:08:06 +0800

    03

---
+++ 03/main.py
@@ -0,0 +1,21 @@
+﻿ 
+a=[i for i in range(0,1001)]
+b=int(input("请输入要查找的数这个数应该在0-1000之间:")) 
+low = 0   
+high = len(a)   
+while(low <= high): 
+    mid = (low + high)//2  
+    midval = a[mid]
+    if midval < b:
+          low = mid + 1   
+    elif midval > b:
+         high = mid - 1  
+    else:
+        print(mid)
+        break
+    
+    	
+
+    
+    
+        
